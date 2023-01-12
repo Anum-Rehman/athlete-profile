e536e7b770c263762030a75d6bf465dc63a23a0a
Ghayyas <ghayyasmubashir@gmail.com>
2023-01-12 20:15:47 +0500

    find by id is working

--- pages/api/[id].js
+++ pages/api/[id].js
@@ -1,4 +1,5 @@
 import clientPromise from "../../lib/mongodb";
+const ObjectId = require('mongodb').ObjectId;
 
 
 /* eslint-disable import/no-anonymous-default-export */
@@ -7,9 +8,11 @@ export default async (req, res) => {
     console.log({ id });
     try {
         const client = await clientPromise;
-        const collection = client.db("athlete").collection("user");
-        const user = await collection.findOne({ _id: id });
-        console.log(user);
+        const db = client.db("athlete");
+        const stringId = id;
+        const objectId = new ObjectId(stringId);
+        const user = await db.collection("user").findOne({ _id: objectId });
+        // const user = await collection.findOne({ "_id": '63c010033bb1842b2e598ed0' });
         res.status(200).json({ status:true, message: user });
     } catch (error) {
         res.status(500).json({ error: error });
