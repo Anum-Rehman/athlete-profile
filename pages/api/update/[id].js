92fd6234e0e7f15e6773c1ff799226a7e0b22dc8
Ghayyas <ghayyasmubashir@gmail.com>
2023-01-12 20:42:51 +0500

    update put api done

---
+++ pages/api/update/[id].js
@@ -0,0 +1,27 @@
+import clientPromise from "../../../lib/mongodb";
+const ObjectId = require('mongodb').ObjectId;
+
+
+/* eslint-disable import/no-anonymous-default-export */
+export default async (req, res) => {
+    const { id } = req.query;
+    try {
+        const client = await clientPromise;
+        const db = client.db("athlete");
+        const stringId = id;
+        const objectId = new ObjectId(stringId);
+        const filter = {
+            _id: objectId
+        }
+        const { name, gender, dob, location, team, about, interest, image, sports } = req.body;
+        console.log({ name, gender, dob, location, team, about, interest, image, sports });
+
+        const update = { $set: { name, gender, dob, location, team, about, interest, image, sports } };
+        const user = await db.collection("user").findOneAndUpdate(filter, update, { returnOriginal: false });
+
+        // const user = await collection.findOne({ "_id": '63c010033bb1842b2e598ed0' });
+        res.status(200).json({ status: true, message: user.value });
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+}
